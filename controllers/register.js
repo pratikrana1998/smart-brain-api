a9fe189c054772d806588b4f45545670500f7d11
pratikrana1998 <pratikrana1998@users.noreply.github.com>
2020-04-23 19:21:23 +0530

    fixes for deployment

--- controllers/register.js
+++ controllers/register.js
@@ -1,34 +1,40 @@
-const handleRegister = (req, res, db, bcrypt) => {
-    const {email, name, password} = req.body;
-    if(!email || !name || !password) {
-        return res.status(400).json('incorrect form submission');
-    }
-    var hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
-            }).then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
+
+const handleRegister = (req, res, bcrypt, db) => {
+  const {name, email, password} = req.body;
+
+  // User validation
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+
+  const hash = bcrypt.hashSync(password);
+
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
     })
-    .catch(err => res.status(404).json('unable to register'));
-   
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+      .returning('*')
+      .insert({
+        name: name,
+        email: loginEmail[0],
+        joined: new Date()
+      })
+      .then(user => {
+        res.json(user[0]);
+      })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json('Unable to register'));
+
 }
 
 module.exports = {
-    handleRegister: handleRegister
+  handleRegister: handleRegister
 };
--- server.js
+++ server.js
@@ -4,57 +4,44 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+//Controllers
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+// database connection
 const db = knex({
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'pratik1433',
-      database : 'smart-brain'
-    }
-  });
+  client: 'pg',
+  connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
+  }
+});
+
+db.select('*').from('users');
+
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send(database.users); 
-})
 
-app.post('/signin', signin.handleSignin(db, bcrypt))
 
-app.post('/register', (req, res) => {
-    register.handleRegister(req, res, db, bcrypt)
-})
+/*-----------------------         ROUTES       -------------------*/
 
-app.get('/profile/:id', (req, res) => {
-    profile.handleProfileGet(req, res, db)
-})
+app.get('/', (req, res) => {res.send('it is working!');});
 
-app.put('/image', (req, res) => {
-    image.handleImage(req, res, db)
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, bcrypt, db)});
 
-app.post('/imageurl', (req, res) => {
-    image.handleApiCall(req, res)
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, bcrypt, db)});
 
-app.listen(process.env.PORT, () => {
-    console.log('app is running on port 3001');
-})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT = user 
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 
-*/
+app.post('/imageurl', (req, res) => {image.handleAPICall(req, res)});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app running in port ${process.env.PORT}`);
+});
